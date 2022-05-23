d067029b42b37b0c9c7abd2a3b8c216e9dcd21f2
Phạm Văn Phi <93720543+PhamVanPhiY@users.noreply.github.com>
2022-05-23 15:29:27 +0700

    upload
    upload

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+// Swiper slider
+var swiper = new Swiper(".bg-slider-thumbs", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var swiper2 = new Swiper(".bg-slider", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  var swiper3 = new Swiper(".mySwiper3", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
